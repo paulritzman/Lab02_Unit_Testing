8b089c9c1ed75fec97e9bf3e858545aaaee4ec2c
paulritzman <ritzman.paul@gmail.com>
2018-06-06 19:16:08 -0700

    Improved ValidateMenuInput functionality - now returns integer

--- ATM/ATM/Program.cs
+++ ATM/ATM/Program.cs
@@ -13,18 +13,21 @@ namespace ATM
         public static void Main(string[] args)
         {
             PrintATMGreeting();
+            PrintMenuOptions();
 
-            string menuSelection = "";
-            bool menuInputValid = false;
-            while (menuInputValid == false)
+            string menuInput = Console.ReadLine();
+            uint menuSelection = ValidateMenuInput(menuInput);
+
+            switch (menuSelection)
             {
-                PrintMenuOptions();
-
-                menuSelection = Console.ReadLine();
-                menuInputValid = ValidateMenuInput(menuSelection);
+                case 1:
+                    ViewBalance();
+                    break;
+                case 2:
+                    PrintWithdrawalMenu();
+                    string withdrawalInput = Console.ReadLine();
+                    break;
             }
-
-            Console.WriteLine("Thanks!");
 
             Console.ReadLine();
         }
@@ -45,37 +48,81 @@ namespace ATM
                 "4) Exit out of the ATM.\n");
         }
 
-        public static bool ValidateMenuInput(string menuInput)
+        public static uint ValidateMenuInput(string menuInput)
         {
+            bool isValid = false;
             uint validSelection = 0;
+
+            while (isValid == false)
+            {
+                try
+                {
+                    validSelection = UInt32.Parse(menuInput);
+
+                    if (validSelection >= 1 && validSelection <= 4)
+                    {
+                        isValid = true;
+                    }
+                    else
+                    {
+                        Console.Clear();
+                        Console.WriteLine("Sorry, that wasn't one of the menu options. " +
+                            "Please type a number between 1 and 4.\n");
+
+                        PrintMenuOptions();
+                        menuInput = Console.ReadLine();
+                    }
+                }
+                catch (Exception e)
+                {
+                    Console.Clear();
+                    Console.WriteLine($"Sorry, that wasn't one of the menu options. {e.Message}\n");
+
+                    PrintMenuOptions();
+                    menuInput = Console.ReadLine();
+                }
+            }
+
+            return UInt32.Parse(menuInput);
+        }
+
+        public static void ViewBalance()
+        {
+            Console.WriteLine($"Your current balance is {balance.ToString("C")}.\n");
+        }
+
+        public static void PrintWithdrawalMenu()
+        {
+            Console.Clear();
+            Console.Write(
+                "How much money would you like to withdrawal?\n" +
+                "(Please enter a positive number, other input will take you back to the main menu.)\n\n" +
+                "Enter withdrawal amount here: ");
+        }
+
+        public static bool ValidateWithdrawalInput(string withdrawalInput)
+        {
+            double validAmount = 0;
             try
             {
-                validSelection = UInt32.Parse(menuInput);
+                validAmount = double.Parse(withdrawalInput);
 
-                if (validSelection >= 1 && validSelection <= 4)
+                if (validAmount > 0.01)
                 {
                     Console.Clear();
                     return true;
                 }
                 else
                 {
-                    Console.WriteLine("\nSorry, that wasn't one of the menu options. " +
-                        "Please type a number between 1 and 4.\n");
-
+                    Console.WriteLine("\nSorry, you can't withdraw less than 1 penny.\n");
                     return false;
                 }
             }
             catch (Exception e)
             {
-                Console.WriteLine($"\nSorry, that wasn't one of the menu options. {e.Message}\n");
-
+                Console.WriteLine($"\nSorry, we're unable to process your transaction. {e.Message}\n");
                 return false;
             }
-        }
-
-        public static void ViewBalance()
-        {
-            Console.WriteLine($"Your current balance is {balance.ToString("C")}.\n");
         }
 
         public static double MakeWithdrawal(double requestAmount)
